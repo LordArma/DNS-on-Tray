26937e17723eec6cd3371be763d470ab6d1e0023
Alireza Ahmadi (Arma) <alirezama@hotmail.com>
2024-05-02 06:56:45 +0330

    Force user to run only one instance of the program in the same time.

--- DNS on Tray/Program.cs
+++ DNS on Tray/Program.cs
@@ -8,10 +8,17 @@ namespace DNS_on_Tray
         [STAThread]
         static void Main()
         {
+            bool result;
+            var mutex = new System.Threading.Mutex(true, "systemontray123", out result);
+
+            if (!result)
+                return;
+
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
             Application.Run(new frmMain());
+            GC.KeepAlive(mutex);
         }
     }
 }
