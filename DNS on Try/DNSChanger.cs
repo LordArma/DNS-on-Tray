69510eb53a13008e3d7d3569874d0a4228328d86
Alireza Ahmadi (Arma) <alirezama@hotmail.com>
2024-05-02 03:54:17 +0330

    fix typo

--- DNS on Try/DNSChanger.cs
+++ DNS on Try/DNSChanger.cs
@@ -1,12 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.DirectoryServices.ActiveDirectory;
-using System.Formats.Tar;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Xml.Linq;
-using Microsoft.Data.Sqlite;
+﻿using Microsoft.Data.Sqlite;
 
 namespace DNS_on_Try
 {
--- DNS on Try/Helper.cs
+++ DNS on Try/Helper.cs
@@ -1,12 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
+﻿using System.ComponentModel;
 using System.Diagnostics;
-using System.Linq;
 using System.Runtime.InteropServices;
 using System.Security.Principal;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace DNS_on_Try
 {
